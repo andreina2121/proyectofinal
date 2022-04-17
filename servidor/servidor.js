496176b31d41fe1115a1468d006afb53f1dae96e
Andreina Pimentel <andreinapimentel@MacBook-Air-de-Andreina.local>
2022-04-17 16:02:42 -0400

    Init commit

---
+++ servidor/servidor.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+
+const publicPath = path.join(__dirname, '../publico'); //configuracion para ir directo a la ruta que queremos, y no tener que salir para volver a entrar
+const port = process.env.PORT || 3002;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3002, () => {
+	console.log(`Servidor conectado en ${port}`);
+});
