639a5fb38b519ceac27319037d9612c7359fa42a
Andreina Pimentel <andreinapimentel@MacBook-Air-de-Andreina.local>
2022-04-17 19:02:45 -0400

    Add connect and disconnect handler

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,14 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname, '../publico'); //configuracion para ir directo a la ruta que queremos, y no tener que salir para volver a entrar
 const port = process.env.PORT || 3002;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('Nueva conexión');
+
+	socket.on('disconnect', () => {
+		console.log('Cliente desconectado');
+	});
+});   //escuchar una conexion
+ 
+server.listen(port, () => {
 	console.log(`Servidor conectado en ${port}`);
 });
