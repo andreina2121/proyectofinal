f8b74b44e720e4f11720a1984bc3fe3b4f3e6e83
Andreina Pimentel <andreinapimentel@MacBook-Air-de-Andreina.local>
2022-04-17 21:05:14 -0400

    create generateMessage utility

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message')
 
 const publicPath = path.join(__dirname, '../publico'); //configuracion para ir directo a la ruta que queremos, y no tener que salir para volver a entrar
 const port = process.env.PORT || 3002;
@@ -17,28 +18,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('Nueva conexión');
 
-	socket.emit('newMessage', {
-		from: 'Admin',
-		text: 'Bienvenido',
-		createdAt: new Date().getTime()
-	});
+	socket.emit('newMessage', generateMessage('Admin', 'Bienvenido'));
 
-	socket.broadcast.emit('newMessage', {
-		from: 'Admin',
-		text: 'New User Joined',
-		createdAt: new Date().getTime()
-
-	});
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'Nuevo cliente'));
 
 
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
 
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		}); 
+		io.emit('newMessage','newMessage', generateMessage(message.from, message.text)); 
 
 
 
